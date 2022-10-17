b2e76f307d53f7d81439e04246079c656288f40e
loizzm <ireliontt@gmail.com>
2022-10-16 23:10:24 -0300

    Alteração na impplemnetação da verificaPermissão

--- Usuario.cpp
+++ Usuario.cpp
@@ -1,4 +1,5 @@
 #include "Usuario.hpp"
+#include <iostream>
 
 Usuario *Usuario::instptrUsuario = 0;
 
@@ -12,12 +13,22 @@ Usuario::Usuario(int _permissao){this->permissao=_permissao;}
 
 
 bool Usuario::verificaPermissao(int _permissao){
-    if (this->permissao == _permissao){
-        return true;
+   try{
+    if ((this->permissao == _permissao)){
+         return true;
     }
+   
         else{
-            return false;
+           
+            throw(permissao);//revisar 
+            
         }
+   }
+   catch(int _p){
+    std::cout<<"Permissão "<<_p<<" não compatível";
+    std::cout<<"\n";
+    return false;
+   }
     }
     
 int Usuario::getPermissao() { return this->permissao; }
