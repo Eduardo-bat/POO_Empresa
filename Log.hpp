6a33698d489625375e02bb3557e46781bdb0d584
loizzm <ireliontt@gmail.com>
2022-10-19 17:21:39 -0300

    classe Log com templates

---
+++ Log.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include "Data.h"
+#include "Usuario.hpp"
+
+template<class t>
+class Log{
+
+protected:
+
+Data data;
+Usuario usuario;
+ t *entidade;
+
+public:
+Log(){};
+ Usuario getUsuario();
+ Data getData();
+ ~Log();
+ void setEntity(t *_entity);
+t getEntity();
+};
+
+
+template <class t>
+ Usuario Log<t>::getUsuario(){return this->usuario;}
+
+template <class t>
+Data Log<t>::getData(){return this->data;}
+
+template <class t>
+void Log<t>:: setEntity(t *_entity){this->entidade=(_entity);}
+
+template <class t>
+t Log<t>::getEntity(){return this->*entidade;}
+
