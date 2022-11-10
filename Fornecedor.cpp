8307ef7fa38dfbfa079f928bc502dad5968ceae3
joaovitor1704 <joaovitor1alves7@gmail.com>
2022-11-10 20:17:03 -0300

    Atualização de fornecedor

--- Fornecedor.cpp
+++ Fornecedor.cpp
@@ -1,13 +1,13 @@
 #include "Fornecedor.hpp"
 
 void Fornecedor::adicionaOrcamento(OrcamentoCompra *orcamento) {
-    respFornecedor.emplace(orcamento)
+    respFornecedor.emplace(orcamento, -1);
 }
 
 float Fornecedor::calculaOrcamento(OrcamentoCompra *orcamento, float valor) {
-
+    respFornecedor.at(orcamento) = valor;
 }
 
 float Fornecedor::respostaOrcamento(OrcamentoCompra *orcamento) {
-
+    return respFornecedor.at(orcamento);
 }
