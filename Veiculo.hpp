ff9ac3eaa02746c1c24d5e32b2906084ee8ffaae
loizzm <ireliontt@gmail.com>
2022-11-19 17:11:24 -0300

    Adiçaõ dos logs e correção de bugs

--- Veiculo.hpp
+++ Veiculo.hpp
@@ -15,8 +15,9 @@ int capacidade;
 Turno *turno;
 std::vector<Funcionario> rota;
 std::pair<float,float> *end_empresa;
+std::string placa;
 public:
-Veiculo(int _capacidade, Turno *_t,std::pair<float,float>*end_empresa);
+Veiculo(int _capacidade, Turno *_t,std::pair<float,float>*end_empresa,std::string placa);
 void adicionafuncionario(Funcionario funcionario);
 void removefuncionario(Funcionario funcionario);
 float calculaDistancia (float x1, float y1, float x2, float y2);
@@ -28,6 +29,9 @@ int getCapacidade();
 Turno getTurno();
 float calculaDistancia_e (float x1, float y1);
 void print_qh();
+void Insere_quadro(Funcionario _f, Data _d);
+void Corrige_horario(Funcionario _f);
+void Arruma_formato(Data *_d,int d,float m,int seg);
 
 
 
